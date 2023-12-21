fa98ea2617e51ce5a18f6d6056c6ea13b7016b6c
aslan.gama <aslan.gama@sixchains.com.br>
2023-12-21 14:34:28 -0300

    nuxt-feather-icons

--- src/runtime/build.js
+++ src/runtime/build.js
@@ -3,14 +3,16 @@ import feather from 'feather-icons'
 import fs from 'fs-extra'
 import pascalcase from 'pascalcase';
 
-const templateComponent = (name) => `
-import feather from 'feather-icons'
+const templateComponent = (name, el) => `
     export default {
         props: {
             size: {
                 type: String,
                 default: '24',
                 validator: (s) => (!isNaN(s) || s.length >= 2 && !isNaN(s.slice(0, s.length - 1)) && s.slice(-1) === 'x')
+            }, 
+            class: {
+                type: String,
             }
         },
         setup(props, {slots}) {
@@ -19,12 +21,11 @@ import feather from 'feather-icons'
                 ? props.size.slice(0, props.size.length - 1) + 'em'
                 : parseInt(props.size) + 'px';
 
-            const content = feather.icons['${name}'].contents;
-            const attrs = feather.icons['${name}'].attrs;
+            const attrs = ${el}
             attrs.width = size
             attrs.height = size
-            attrs.innerHTML = content
-
+            attrs.class = attrs.class+' '+props.class
+                       
             return () => [
                 h('svg', attrs)
             ]
@@ -39,7 +40,11 @@ const build = Object.keys(feather.icons).map(name => ({
 }))
 
 Promise.all(build.map(icon => {
-    const component = templateComponent(icon.name)
+    const content = feather.icons[icon.name].contents;
+    const el = feather.icons[icon.name].attrs;
+    el.innerHTML = content
+
+    const component = templateComponent(icon.name, JSON.stringify(el))
     const filepath = `./runtime/components/${icon.componentPascalName}.js`
     return fs.ensureDir(path.dirname(filepath))
         .then(() => fs.writeFile(filepath, component, 'utf8'))
