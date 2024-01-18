660950ab408f20dd0bb6a64709f8c3fe620ac382
aslan.gama <aslan.gama@sixchains.com.br>
2024-01-18 09:50:05 -0300

    Nuxt-feather-icons
    - playground

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,9 @@
 module.exports = {
-  root: true,
-  extends: ["@nuxt/eslint-config"],
+    root: true,
+    extends: [
+        "@nuxt/eslint-config"
+    ],
+    rules: {
+        "no-unused-vars": "off",
+    }
 };
