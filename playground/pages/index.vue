8c780f2315eb0cedc3195bb497664b364ca73abf
aslan.gama <aslan.gama@sixchains.com.br>
2024-11-22 00:56:20 -0300

    - playground

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -1,7 +1,25 @@
-<script setup lang="ts">
-
+<script setup>
+const menuItems = ref([
+  {
+    icon: resolveComponent('HomeIcon'),
+  },
+  {
+    icon: resolveComponent('UsersIcon'),
+  },
+  {
+    icon: resolveComponent('LayersIcon'),
+  },
+])
 </script>
 
 <template>
-  <NuxtWelcome></NuxtWelcome>
+  <div>
+    <XIcon size="30" class="custom-class"></XIcon>
+
+    <ul>
+      <li v-for="(item, index) in menuItems" :key="index">
+        <component :is="item.icon" size="2.5x"/>
+      </li>
+    </ul>
+  </div>
 </template>
