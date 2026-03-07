291d0fa65ed136d29180d8919ebe4c5199c396e2
aslan.gama <aslan.gama@sixchains.com.br>
2026-03-07 16:51:36 -0300

    chore(nuxt-feather-icons): Update Nuxt configuration and TypeScript references

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,8 +1,9 @@
+import myModule from '../src/module';
 export default defineNuxtConfig({
     devtools: {
         enabled: true
     },
     modules: [
-        "nuxt-feather-icons"
+        myModule
     ]
 })
