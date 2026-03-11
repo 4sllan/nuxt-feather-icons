9df0e48f079f5d97b2983801d784e9378f1c00c0
aslan.gama <aslan.gama@sixchains.com.br>
2026-03-11 10:17:45 -0300

    docs(nuxt-feather-icons): document default Icon properties - explain default configuration values

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -9,7 +9,10 @@ export default defineNuxtConfig({
         myModule
     ],
     nuxtFeatherIcons: {
-        // prefix: 'F'
+        // prefix: 'F',
+        // size: '24px', // default <Icon> size applied
+        // class: 'icon', // default <Icon> class applied
+        // strokeWidth: '2'
     },
     compatibilityDate: '2026-03-07'
 })
