6fd5bd854e73032087054576ebe0eb14adf1b2d8
aslan.gama <aslan.gama@sixchains.com.br>
2026-03-11 11:37:54 -0300

    chore(nuxt-feather-icons): remove commented configuration options
    - remove commented size configuration
    - remove commented class configuration
    - remove commented strokeWidth configuration

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -10,9 +10,6 @@ export default defineNuxtConfig({
     ],
     nuxtFeatherIcons: {
         // prefix: 'F',
-        // size: '24px', // default <Icon> size applied
-        // class: 'icon', // default <Icon> class applied
-        // strokeWidth: '2'
     },
     compatibilityDate: '2026-03-07'
 })
